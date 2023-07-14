079b5eca248e13c6000886571ff1eeee7841caac
Uladzislau Yatsko <yatsko.vladislav@gmail.com>
2023-07-14 16:41:57 +0200

    Home task with file writing done.

---
+++ modules_files/classes.py
@@ -0,0 +1,143 @@
+import os
+import sys
+from datetime import date
+
+
+class News:
+    def __init__(self):
+        self._text_of_news = input('Please specify your news: ')
+        self._city = input('Please specify your city: ')
+        self._date_of_post = date.today().strftime('%d-%m-%Y')
+    
+    @property
+    def text_of_news(self):
+        return self._text_of_news
+    
+    @property
+    def city(self):
+        return self._city
+    
+    @property
+    def date_of_post(self):
+        return self._date_of_post
+    
+    @text_of_news.setter
+    def text_of_news(self, value):
+        self._text_of_news = value
+    
+    @city.setter
+    def city(self, value):
+        self._city = value
+    
+    def post_new_evidence(self, text_of_news: str, city: str, date_of_post) -> None:
+        with open('text_with_output', 'a') as file:
+            file.write(('-' * 10) + '\n')
+            file.write('News: \n')
+            file.write(text_of_news + '\n')
+            file.write(f'Issued city: {city} \t Date_of_post: {date_of_post} \n')
+            file.write(('-' * 10) + '\n')
+        return
+
+
+class Advertising:
+    def __init__(self):
+        self._ad_text = input('Please specify your ad: ')
+        self._expiration_date_str = input('Please specify expiration date in yyyy-mm-dd format: ')
+        self._expiration_date = date.fromisoformat(self._expiration_date_str)
+    
+    @property
+    def ad_text(self):
+        return self._ad_text
+    
+    @property
+    def expiration_date(self):
+        return self._expiration_date
+    
+    @ad_text.setter
+    def ad_text(self, value):
+        self._ad_text = value
+    
+    def calculate_days_to_expire(self):
+        if (self._expiration_date - date.today()).days <= 0:
+            return 'Expired'
+        else:
+            return (self._expiration_date - date.today()).days
+    
+    def post_your_ad(self, ad_text: str, expiration_date) -> None:
+        with open('text_with_output', 'a') as file:
+            file.write(('-' * 10) + '\n')
+            file.write('Ad: \n')
+            file.write(ad_text + '\n')
+            file.write(f'Days_to_expire: {expiration_date} \n')
+            file.write(('-' * 10) + '\n')
+        return
+
+
+class TwitterPost(News, Advertising):
+    def __init__(self):
+        super().__init__()
+        self._post_text = input('Specify text of your post: ')
+        self._user_name = input('Specify your username: ')
+        self._customer_name = input('Specify customer name: ')
+    
+    @property
+    def post_text(self):
+        return self._post_text
+    
+    @post_text.setter
+    def post_text(self, value):
+        self._post_text = value
+    
+    @property
+    def user_name(self):
+        return self._user_name
+    
+    @user_name.setter
+    def user_name(self, value):
+        self._user_name = value
+    
+    def post_your_ideas(self, user_name: str, post_text: str, date_of_post) -> None:
+        with open('text_with_output', 'a') as file:
+            file.write(('-' * 10) + '\n')
+            file.write(f'Posted by {user_name}: \n')
+            file.write(post_text + '\n')
+            file.write(f'Date_of_post: {date_of_post} \n')
+            file.write(('-' * 10) + '\n')
+        return
+    
+    def post_new_evidence(self, text_of_news: str, city: str, date_of_post) -> None:
+        with open('text_with_output', 'a') as file:
+            file.write(('-' * 10) + '\n')
+            file.write(f'Posted by {self._user_name}. Please specify your_news: \n')
+            file.write(text_of_news + '\n')
+            file.write(f'Issued city: {city} \t Date_of_post: {date_of_post} \n')
+            file.write(('-' * 10) + '\n')
+        return
+    
+    def post_your_ad(self, ad_text: str, expiration_date) -> None:
+        with open('text_with_output', 'a') as file:
+            file.write(('-' * 10) + '\n')
+            file.write('Ad: ')
+            file.write(f'Customer is {self._customer_name}: ')
+            file.write(ad_text + '\n')
+            file.write(f'Days_to_expire: {expiration_date} \n')
+            file.write(('-' * 10) + '\n')
+        return
+
+class FileReader:
+    def read_file_with_items(self, path=f'{sys.path[0]}\\text_to_input.txt') -> str:
+        with open(path, 'r') as file_to_read:
+            text = file_to_read.read()
+            if text.startswith('-' * 10 + '\n') and text.endswith('-' * 10 + '\n'):
+                return text
+            else:
+                return 'Format mismatch!'
+            
+    def write_file_from_another(self, text: str) -> None:
+        with open('text_with_output', 'a') as file:
+            file.write(text)
+            return
+    
+    def delete_input_file(self, path=f'{sys.path[0]}\\text_to_input.txt') -> None:
+        os.remove(path)
+        return
---
+++ modules_files/tool_for_text.py
@@ -0,0 +1,112 @@
+import classes
+from functions import strings_to_functions
+
+def specify_choice(user_selection):
+    match user_selection:
+        case '1':
+            news = classes.News()
+            print('You have 2 options. Post as it is (1) or update before sending (2)')
+            action = input('Specify your action (1-2): ')
+            if action == '1':
+                news.post_new_evidence(news.text_of_news, news.city, news.date_of_post)
+                print('Your news are posted to text_with_output file. Good Job!')
+            else:
+                news.text_of_news = input('Please update the text: ')
+                news.city = input('Please update the city: ')
+                news.post_new_evidence(news.text_of_news, news.city, news.date_of_post)
+        case '2':
+            advertising = classes.Advertising()
+            print('You have 2 options. Post as it is (1) or update before sending (2)')
+            action = input('Specify your action (1-2): ')
+            if action == '1':
+                days_to_expire = advertising.calculate_days_to_expire()
+                advertising.post_your_ad(advertising.ad_text, days_to_expire)
+                print('Your ad is posted to text_with_output file. Good Job!')
+            else:
+                advertising.ad_text = input('Please update the text: ')
+                days_to_expire = advertising.calculate_days_to_expire()
+                advertising.post_your_ad(advertising.ad_text, days_to_expire)
+        case '3':
+            twitter = classes.TwitterPost()
+            print('You have 3 options to do with your twitter post:\n'
+                  '1. Send a news\n'
+                  '2. Place an advertising\n'
+                  '3. Share your own ideas in your post\n')
+            type_of_post = input('Please specify your post type: ')
+            match type_of_post:
+                case '1':
+                    print('You have 2 options. Post as it is (1) or update before sending (2)')
+                    action = input('Specify your action (1-2): ')
+                    if action == '1':
+                        twitter.post_new_evidence(twitter.text_of_news, twitter.city, twitter.date_of_post)
+                        print('Your news are posted to text_with_output file. Good Job!')
+                    else:
+                        twitter.text_of_news = input('Please update the text: ')
+                        twitter.city = input('Please update the city: ')
+                        twitter.post_new_evidence(twitter.text_of_news, twitter.city, twitter.date_of_post)
+                        print('Your news are posted to text_with_output file. Good Job!')
+                case '2':
+                    print('You have 2 options. Post as it is (1) or update before sending (2)')
+                    action = input('Specify your action (1-2): ')
+                    if action == '1':
+                        days_to_expire = twitter.calculate_days_to_expire()
+                        twitter.post_your_ad(twitter.ad_text, days_to_expire)
+                        print('Your ad is posted to text_with_output file. Good Job!')
+                    else:
+                        twitter.ad_text = input('Please update the text: ')
+                        days_to_expire = twitter.calculate_days_to_expire()
+                        twitter.post_your_ad(twitter.ad_text, days_to_expire)
+                        print('Your ad is posted to text_with_output file. Good Job!')
+                case '3':
+                    print('You have 2 options. Post as it is (1) or update before sending (2)')
+                    action = input('Specify your action (1-2): ')
+                    if action == '1':
+                        twitter.post_your_ideas(twitter.user_name, twitter.post_text, twitter.date_of_post)
+                        print('Your ideas are posted to text_with_output file. Good Job!')
+                    else:
+                        twitter.ad_text = input('Please update the text: ')
+                        twitter.user_name = input('Please update the username')
+                        twitter.post_your_ideas(twitter.user_name, twitter.post_text, twitter.date_of_post)
+                        print('Your ideas are posted to text_with_output file. Good Job!')
+        case '4':
+            reader = classes.FileReader()
+            print('You have 2 options: \n'
+                  '1. Write from file with current folder. \n'
+                  '2. specify path on your own.')
+            user_option = input('Please specify your option: ')
+            match user_option:
+                case '1':
+                    text = reader.read_file_with_items()
+                    if text != 'Format mismatch!':
+                        normalized_text = strings_to_functions.normalize_text(reader.read_file_with_items())
+                        reader.write_file_from_another(normalized_text)
+                        reader.delete_input_file()
+                    else:
+                        print('Incorrect text format!')
+                case '2':
+                    path_to_file = input('Please specify your path\\to\\file: ')
+                    text = reader.read_file_with_items()
+                    if text != 'Format mismatch!':
+                        normalized_text = strings_to_functions.normalize_text(reader.read_file_with_items(path_to_file))
+                        reader.write_file_from_another(normalized_text)
+                        reader.delete_input_file(path_to_file)
+                    else:
+                        print('Incorrect text format!')
+        
+        case _:
+            print('Wrong parameter! Please select from 1 to 5')
+
+
+while True:
+    print('Welcome! Here you can get some stuff for you. Here is options list.\n'
+          'Use numbers to select proper option:\n'
+          '1. News\n'
+          '2. Advertising\n'
+          '3. Twitter post\n'
+          '4. Input from file\n'
+          '5. Exit.')
+    user_choice = input('Please specify your option from 1 to 5: ')
+    if user_choice == '5':
+        print('Thank you for your time!')
+        break
+    specify_choice(user_choice)
