d06554316b300bdd1751d58336044a50e9ab89a1
Uladzislau Yatsko <yatsko.vladislav@gmail.com>
2023-07-17 17:01:18 +0200

    Home task with DB done.

---
+++ database_task/database_initialization.py
@@ -0,0 +1,16 @@
+import pyodbc
+
+connection = pyodbc.connect('DRIVER={SQLite3 ODBC Driver};'
+                            'Direct=True;'
+                            'Database=test.db;'
+                            'String Types=Unicode')
+
+cursor = connection.cursor()
+cursor.execute('CREATE TABLE news (news text, city text, date_of_post text)')
+cursor.execute('CREATE TABLE ads (ad_text text, expiration_date text)')
+cursor.execute('CREATE TABLE twits (user_name text, post_text text, date_of_post text)')
+
+connection.commit()
+
+cursor.close()
+connection.close()
